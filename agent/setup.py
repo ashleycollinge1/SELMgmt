916d707fd2bb6a9d6c630f91ed8f4dfe65e41c14
Ashley Collinge <ashley.collinge@outlook.com>
2018-09-12 15:34:27 +0100

    setup.py complete for msi build

---
+++ agent/setup.py
@@ -0,0 +1,12 @@
+#setup.py
+from cx_Freeze import setup, Executable
+
+setup(
+    name = "SELAgent",
+    version = "0.0.3",
+    options = {"build_exe": {
+        'include_files': ['connected.ico', 'disconnected.ico'],
+        'include_msvcr': True,},
+        "bdist_msi": {'upgrade_code': '{66620F3A-DC3A-11E2-B341-002219E9B01E}',}},
+    executables = [Executable("run_agent.py",base="Win32GUI")]
+    )
