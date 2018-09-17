48ac979b4a762a03cf0e99209e27a827cb61e82b
Ashley Collinge <ashley.collinge@outlook.com>
2018-09-17 14:01:14 +0100

    added xml reporting for tests

--- server/run_testsuite.py
+++ server/run_testsuite.py
@@ -2,13 +2,14 @@
 Run all of the tests from here
 """
 import unittest
+import xmlrunner
 from testing.testsuite import WebApp
 
 def main():
     """
     Run the unittests
     """
-    unittest.main()
+    unittest.main(testRunner=xmlrunner.XMLTestRunner(output='test-reports'))
 
 if __name__ == '__main__':
     main()
